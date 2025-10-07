5a2fbf492125be1c190ded4ae8f934120e220188
charujain0 <charujain1411@gmail.com>
2025-10-07 22:57:46 +0530

    Add files via upload

---
+++ tash1.py
@@ -0,0 +1,8 @@
+# Get an integer from the user
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd using an if-else statement
+if num % 2 == 0:
+    print(str(num) + " is an even number.")
+else:
+    print(str(num) + " is an odd number.")
